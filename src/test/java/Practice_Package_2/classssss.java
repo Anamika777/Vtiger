daa2400d92ffbd9b3831751a97ab512442bf16ab
B.P.KUSHWAHA <B.P.KUSHWAHA@DESKTOP-LVNM1PD>
2023-01-19 16:00:33 +0530

    integer class

---
+++ src/test/java/Practice_Package_2/classssss.java
@@ -0,0 +1,16 @@
+package Practice_Package_2;
+
+import java.util.Scanner;
+
+public class classssss {
+
+	public static void main(String[] args) {
+	     try (Scanner sc = new Scanner(System.in)) {
+	 		int num=sc.nextInt();
+	 		   System.out.println(num);
+	 	}
+	 	}
+
+	}
+
+
